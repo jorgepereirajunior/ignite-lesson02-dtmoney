a9e9ede88c1977b1b66201fc9cbe6dac5a1da8b8
Jorginho <jorgejunior.jpj2016@gmail.com>
2021-08-04 00:35:53 -0300

    :lipstick: style: Create Header style

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  padding: 2rem 1rem 12rem;
+  background: var(--blue);
+`
+
+export const Content = styled.div`
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  max-width: 1120px;
+
+  button {
+    padding: 0 2rem;
+    border: 0;
+    border-radius: 0.25rem;
+
+    height: 3rem;
+
+    font-size: 1rem;
+    color: #FFF;
+    background: var(--blue-light);
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`
