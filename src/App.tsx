1c317214594daf30c265c4d57afdaedcdadbcba7
Jorginho <jorgejunior.jpj2016@gmail.com>
2021-08-05 22:20:05 -0300

    :building_construction: chore: Import Dashboard Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Header } from "./components/Header"
-import { GlobalStyle } from "./styles/global"
+import { Dashboard } from "./components/Dashboard"
 
+import { GlobalStyle } from "./styles/global"
 
 export function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
+      <Dashboard />
     </>
   )
 }
