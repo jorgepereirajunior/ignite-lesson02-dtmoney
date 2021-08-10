83cb420e55e414fb14efb42c6bdcfc622db19876
Jorginho <jorgejunior.jpj2016@gmail.com>
2021-08-09 23:27:29 -0300

    :sparkles: feat(TransactionContext) Adds function createTransaction to Transaction Context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -11,12 +11,19 @@ interface Transaction {
   createdAt: string
 }
 
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
 interface TransactionProviderProps {
   children: ReactNode
 }
 
+interface TransactionContextFormat {
+  transactions: Transaction[]
+  createTransaction: (transaction: TransactionInput) => Promise<void>
+}
 
-export const TransactionsContext = createContext<Transaction[]>([])
+
+export const TransactionsContext = createContext<TransactionContextFormat>({} as TransactionContextFormat)
 
 
 export function TransactionsProvider({ children }: TransactionProviderProps) {
@@ -26,9 +33,23 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     api.get('transactions')
       .then(response => setTransactions(response.data.transactions))
   }, [])
+
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date()
+    })
+
+    const { transaction } = response.data
+
+    setTransactions([
+      ...transactions,
+      transaction
+    ])
+  }
   
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   )
